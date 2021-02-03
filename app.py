c994bdd4e767c838f5ed2f24dadf80388175a10a
SilvioCresci <silviocresci1996@gmail.com>
2021-02-03 16:27:59 +0100

    aa

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def init_func(argv):
     APP = web.Application(middlewares=[aiohttp_error_middleware])
     APP.router.add_post("/api/messages", messages)
     return APP
-if name == "__main__":
+if __name__ == "__main__":
     APP = init_func(None)
     try:
         web.run_app(APP, host="localhost", port=CONFIG.PORT)
